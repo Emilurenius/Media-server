a1e965e1da42ffd90ad769a8e7f952332a4ee0c6
Emil Christiansen <emilsen68@gmail.com>
2022-02-04 19:52:35 +0100

    Same filename check
    If an image with the same name as an already existing one is uploaded, the image filename is given an underscore, so it can be saved in the same folder. Also fixed a vulnerability that made it possible to crash the server with one POST request

--- server/app.js
+++ server/app.js
@@ -6,13 +6,15 @@ const fs = require('fs')
 const cors = require('cors')
 const fileUpload = require("express-fileupload");
 
+const categories = [
+  'felles-kvelder',
+  'ukategorisert'
+]
+
 // Reading input from terminal start
 const port = parseInt(process.argv[2]) || 3000
 console.log(`${port} registered as server port`)
 // Reading input from terminal end
-
-
-
 
 
 app.use(cors()) // Making sure the browser can request more data after it is loaded on the client computer.
@@ -22,8 +24,23 @@ app.use('/static', express.static('public'))
 
 
 app.post('/upload', function(req, res) {
-  console.log(req.files.foo); // the uploaded file object
-  res.send('Success');
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded');
+  }
+
+  else if (!categories.includes(req.body.category)) {
+    return res.status(400).send('Category does not exist')
+  }
+
+  const existingImages = fs.readdirSync(path.join(__dirname, `/images/${req.body.category}`))
+  let fileName = req.files.sampleFile.name.split('.')
+  while (existingImages.includes(`${fileName[0]}.${fileName[1]}`)) {
+    fileName[0] = `${fileName[0]}_`
+  }
+
+  req.files.sampleFile.mv(path.join(__dirname, `/images/${req.body.category}/${fileName[0]}.${fileName[1]}`))
+  return res.status(200).send('Image uploaded successfully')
 });
 
 
