676fd380a84c2852a242146062d4dcfa6f75a26c
Emil Christiansen <emilsen68@gmail.com>
2022-02-05 15:26:05 +0100

    Cannot double click upload
    If you press upload while uploading, another upload will not begin

--- client/index/src/App.js
+++ client/index/src/App.js
@@ -17,6 +17,7 @@ const App = () => {
   const [filesAmount, setFilesAmount] = useState(`0 Filer valgt`)
   const [category, setCategory] = useState('ukategorisert')
   const [progress, setProgress] = useState('')
+  const [uploading, setUploading] = useState(false)
   const [fileInput, setFileInput] = useState(null)
 
   const fileSelectedHandler = event => {
@@ -29,6 +30,10 @@ const App = () => {
   }
 
   const fileUploadHandler = () => {
+    setUploading(true)
+    if (uploading) {
+      return
+    }
     const fd = new FormData()
     fd.append('category', category)
     for (let i=0;i<selectedFiles.length;i++) {
@@ -42,10 +47,12 @@ const App = () => {
       .then(res => {
         console.log(res)
         setProgress('Opplasting ferdig')
+        setUploading(false)
       })
       .catch(err => {
         console.log(err)
         setProgress('Oida! Noe gikk galt. Hvis problemet fortsetter, si ifra i skap chatten!')
+        setUploading(false)
       })
   }
 
